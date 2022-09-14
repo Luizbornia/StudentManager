f355352a7d54f182c7528609fb1f0b583ea9b347
Luizbornia <luizgiobornia@hotmail.com>
2022-09-14 20:34:09 -0300

    Commit de mvc Nota

--- src/main/java/com/api/gerenciador/models/NotaModel.java
+++ src/main/java/com/api/gerenciador/models/NotaModel.java
@@ -1,7 +1,15 @@
 package com.api.gerenciador.models;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name="Notas")
